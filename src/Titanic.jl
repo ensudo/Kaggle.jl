13b0dee6ec02346e932cd9188fad22be93ce2258
adebesin <greade01@C02X70WNJHD5.manchester.arm.com>
2019-05-14 13:59:01 +0100

    Flux logistic regression model

--- src/Titanic.jl
+++ src/Titanic.jl
@@ -1,10 +1,10 @@
-import Flux: onehot, onehotbatch, normalise
+import Flux: onehot, onecold, onehotbatch, normalise, Data, Descent, Dense, Chain, softmax, crossentropy, train!, params
 import Calculus
 import GLM
 import CSV: File
 import MLLabelUtils: label, labelmap, convertlabel
 import DataFrames: first, DataFrame, describe
-import StatsPlots: plot
+import StatsPlots: plot, plotlyjs, plot, heatmap
 import Knet
 import StatsBase
 import Distributions
@@ -12,24 +12,66 @@ import DataFramesMeta
 import MultivariateStats
 import Statistics: mean
 import Missings: skipmissing, replace
+import MLDataUtils: splitobs
 
+#[:, start:stride:end]
 
-statsplots.plotlyjs()
+plotlyjs()
 
 train = File("../resource/train.csv") |> DataFrame
 
 train.Sex = convert(Array{String, 1}, train.Sex)
 
+labels = train[:Survived]
+features = train[[:Sex, :Age, :SibSp, :Parch]]
+
 meanage = train.Age |> skipmissing |> mean |> round
+features.Age = replace(features.Age, meanage) |> collect
+features.Sex = convertlabel([1, -1], features.Sex)
 
-train.Age = replace(train.Age, meanage) |> collect
+features = Matrix(features) |> transpose
 
-train.Sex = convertlabel([1, -1], train.Sex)
+normedfeatures = normalise(features, dims=2)
+
+klasses = unique(labels) |> sort
+onehotlabels = onehotbatch(labels, klasses)
+
+# xtrain = splitobs(normedfeatures, at=0.7)[1]
+# ytrain = splitobs(onehotlabels, at=0.7)[1]
+
+trainindices = [1:2:891 ; 2:2:891]
+xtrain = normedfeatures[:, trainindices]
+ytrain = onehotlabels[:, trainindices]
 
 
+xtest = normedfeatures[:, 3:2:150]
+ytest = onehotlabels[:, 3:2:150]
 
-first(train, 10)
 
-describe(train)
+model = Chain(
+    Dense(4, 2),# xrows, yrows
+    softmax
+)
+
+loss(x, y) = crossentropy(model(x), y)
+optimiser = Descent(0.5)
+dataiterator = Iterators.repeated((xtrain, ytrain), 110)
+
+train!(loss, params(model), dataiterator, optimiser)
+accuracy(x, y) = mean(onecold(model(x)) .== onecold(y))
+
+score = accuracy(xtest, ytest)
+
+@assert score > 0.8
+
+
+function confusionmatrix(X, y)
+    ŷ = onehotbatch(onecold(model(X)), 1:3)
+    y * ŷ'
+end
+
+cmat = confusionmatrix(xtest, ytest)
+
+display(cmat)
 
 
